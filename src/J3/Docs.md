bedafd3ec70c6dd4521b515c1419fbba0e6baed7
Faraci Tony <faracitonyp@gmail.com>
2021-11-19 09:07:06 +0100

    m

---
+++ src/J3/Docs.md
@@ -0,0 +1,9 @@
+caractéristique d'un SUDOKU
+
+La classe Case a pour attribut :
+
+position : elle nous permet de voir la position de la case dans la ligne 
+value : permet de connaitre la valeur de la case 
+
+position = pos 
+value = value 
---
+++ src/J3/stack.py
@@ -0,0 +1,94 @@
+class Stack:
+    
+    def __init__(self):
+        """
+            Initialisation de la pile
+            
+            Tests :
+            >>> p = Stack()
+            >>> p.stack == []
+            True
+            >>> p.top == None
+            True
+        """
+        pass
+    
+        self.stack = []
+        self.top = None
+    
+        
+    def push(self, el):
+        """
+            Ajouter un élément à la pile
+            
+            Tests :
+            >>> p = Stack()
+            >>> p.push(13)
+            >>> p.push((12, 1, 9))
+            >>> p.stack == [13, (12, 1, 9)]
+            True
+            >>> p.top == 13
+            False
+            >>> p.top == (12, 1, 9)
+            True
+        """
+        pass
+        self.stack.append(el)
+        self.top = el
+    
+    
+    def empty(self):
+        """
+            Vérifie si la pile est vide
+            
+            Tests :
+            >>> p = Stack()
+            >>> p.empty()
+            True
+            >>> p.push(12)
+            >>> p.empty()
+            False
+        """
+        pass
+    
+        return len(self.stack) == 0
+    
+        
+    def pop(self):
+        """
+            Retirer et renvoyer si possible l'élément en tête de pile
+            
+            Tests :
+            >>> p = Stack()
+            >>> p.pop()
+            >>> p.push(13)
+            >>> p.pop()
+            13
+            >>> p.push(13)
+            >>> p.push((1, 2, 3))
+            >>> p.push("NSI")
+            >>> p.top == "NSI"
+            True
+            >>> p.pop()
+            'NSI'
+            >>> p.top == (1, 2, 3)
+            True
+            >>> p.pop()
+            (1, 2, 3)
+            >>> p.pop()
+            13
+        """
+        pass
+    
+        if not self.empty():
+            el = self.stack.pop()
+            if not self.empty():
+                self.top = self.stack[-1]
+            else:
+                self.top = None
+            return el
+        return None
+        
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
